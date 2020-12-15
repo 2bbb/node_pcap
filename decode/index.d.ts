80646cc38fd37c010c9ada9e4acfa21ef08f230d
2bit@M1 <github@2bit.jp>
2020-12-15 12:41:56 +0900

    clean up codes

--- decode/index.d.ts
+++ decode/index.d.ts
@@ -1,21 +1,15 @@
 import { PacketWithHeader } from 'pcap';
 
-import EthernetPacketType = require("./ethernet_packet");
-import IPv4 = require("./ipv4");
-import IPv6 = require("./ipv6");
-import Arp = require("./arp");
-import PcapPacketType = require("./pcap_packet");
+export import PcapPacket = require("./pcap_packet");
+export import EthernetPacket = require("./ethernet_packet");
+export import IPv4Packet = require("./ipv4");
+export import IPv6Packet = require("./ipv6");
+export import ArpPacket = require("./arp");
 import { PcapPacketDecodeOptions } from '../types/pcap_packet_decode_options';
 
-export const EthernetPacket: typeof EthernetPacketType;
-export const IPv4Packet: typeof IPv4;
-export const IPv6Packet: typeof IPv6;
-export const ArpPacket: typeof Arp;
-export const PcapPacket: typeof PcapPacketType;
-
 interface Decode {
-    (packet: PacketWithHeader, emitter?: Function, options?: PcapPacketDecodeOptions): PcapPacketType;
-    packet(packet: PacketWithHeader, emitter?: Function, options?: PcapPacketDecodeOptions): PcapPacketType;
+    (packet: PacketWithHeader, emitter?: EventEmitter, options?: PcapPacketDecodeOptions): PcapPacketType;
+    packet(packet: PacketWithHeader, emitter?: EventEmitter, options?: PcapPacketDecodeOptions): PcapPacketType;
 }
 
 export const decode: Decode;
